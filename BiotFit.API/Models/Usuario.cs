f081c269c425375ac0be97829dcd58e542cb5343
EduardoSanchesSantos <127109184+EduardoSanches03@users.noreply.github.com>
2025-10-17 00:53:01 -0300

    Start

---
+++ BiotFit.API/Models/Usuario.cs
@@ -0,0 +1,9 @@
+namespace BiotFit.API.Models
+{
+    public class Usuario
+    {
+          public string Id { get; set; }
+        public string UserName  { get; set; }
+        public string Password {  get; set; }
+    }
+}
